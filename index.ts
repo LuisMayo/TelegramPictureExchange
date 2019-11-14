f4897c6345a3a862787d19b208933a1a298bce71
Luis Mayo Valbuena <luismayovalbuena@outlook.es>
2019-11-14 19:35:50 +0000

    Initial commit

---
+++ index.ts
@@ -0,0 +1,38 @@
+import * as fs from 'fs';
+import { PhotoSize } from 'telegraf/typings/telegram-types';
+import * as Telegraf from 'telegraf';
+
+type Conf = {
+    token: string;
+}
+
+let lastChat: number;
+let lastPic: string;
+
+// const Telegraf = require('telegraf');
+const conf: Conf = JSON.parse(fs.readFileSync('./conf.json', {encoding: 'UTF-8'}));
+const bot = new Telegraf.default(conf.token);
+
+bot.start(ctx => {
+    ctx.reply('Hi! This bot can be used for exchanging pictures with random users!\nTo start just sent me a picture, caption can be provided')
+});
+bot.on('photo', (ctx) => {
+    let bestPhoto: PhotoSize;
+    for (const photo of ctx.message.photo) {
+        if(!bestPhoto || bestPhoto.file_size < photo.file_size) {
+            bestPhoto = photo;
+        }
+    }
+
+    if(lastChat) {
+        bot.telegram.sendPhoto(lastChat, bestPhoto.file_id);
+        bot.telegram.sendPhoto(ctx.chat.id, lastPic);
+        lastChat = lastPic = null;
+    } else {
+        lastChat = ctx.chat.id;
+        lastPic = bestPhoto.file_id;
+        ctx.reply('Waiting for another user to upload their photo');
+    }
+});
+
+bot.launch();
