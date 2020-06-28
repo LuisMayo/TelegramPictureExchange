6ecd4d18fa0ba8434f5234eb20cddf8bc48f41e6
LuisMayo <luismayovalbuena@outlook.es>
2020-06-28 19:26:43 +0200

    Updated button-keyboard-helper

--- src/button-keyboard-helper.ts
+++ src/button-keyboard-helper.ts
@@ -1,18 +1,19 @@
 import * as Telegraf from 'telegraf';
 
 export class ButtonKeyBoardHelper {
-    buttons: Telegraf.CallbackButton[][] = [];
+    buttons: (Telegraf.CallbackButton | Telegraf.Button)[][] = [];
     lastArr = [];
     charactersInCurrentLine = 0;
 
     constructor() {
         this.buttons.push(this.lastArr);
     }
-    addNewButton(text: string) {
-        if (this.charactersInCurrentLine + text.length >= 40) {
+    addNewButton(text: string, data?: string | boolean) {
+        const callback = !(data == null || typeof data === 'boolean');
+        if (this.charactersInCurrentLine + text.length >= (callback ? 30 : 50)) {
             this.newLine();
         }
-        this.lastArr.push(Telegraf.Markup.button(text));
+        this.lastArr.push(callback ? Telegraf.Markup.callbackButton(text, data as string) : Telegraf.Markup.button(text, data as boolean));
         this.charactersInCurrentLine+=text.length;
     }
 
